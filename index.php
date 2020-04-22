1da4c4e70d2d9a9849507257c82cc5ad58279e05
francesco-pecora <francescogp9898@hotmail.com>
2020-04-22 00:17:21 -0400

    basic front-end completed

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaina+2&display=swap" rel="stylesheet">
+    <title>Cerco</title>
+</head>
+<body>
+    <div class="wrapper indexPage">
+        <div class="mainSection">
+            <div class="logoContainer">
+                <img src="./assets/images/cerco-logo.png" alt="">
+            </div>
+            <div class="searchContainer">
+                <form action="search.php" method="GET">
+                    <input class="searchBox" type="text" name="term">
+                    <input class="searchButton" type="submit" value="Search">
+                </form>       
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaina+2&display=swap" rel="stylesheet">
+    <title>Cerco</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="headerContent">
+                
+                <div class="logoContainer">
+                    <a href="index.php"><img src="./assets/images/cerco-logo.png" alt=""></a>
+                </div>
+                
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+                        <div class="searchBarContainer">
+                            <input class="searchBox" type="text" name="term"/>
+                            <button class="searchButton">
+                                <img src="assets/images/magnifying-glass-search.png"/>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
